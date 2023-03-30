83ad9f495dec603077ff891d0ccb53af9e6ecdfa
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 16:02:32 -0400

    add in the props to the squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,22 +1,31 @@
-export default function Board() {
+//5 import the sqaure from the components and now replace what we had before with the "Square" components
+import Square from "./components/Square"
+
+
+ function Board() {
   //3 note that you need to put children tags within a parent tag as you see here 
   return (
     <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+    {/* 7 now we want to add in the props value with the number, this is like our FUNCTION call with an argument */}
+    <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   )
 }
+
+
+
+export default Board
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,3 +1,9 @@
-function Square() {
-    return <button className="square">1</button>;
+// 4 Create a new Sqaure component
+
+
+//6 now we need to pass in the value props
+function Square({value}) {
+    return <button className="square">{value}</button>;
   }
+
+export default Square
