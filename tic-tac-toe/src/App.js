ab0de6df54d80cdf36b3a9e09e93e10a131a5274
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 15:50:17 -0400

    added in squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,3 +1,22 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  //3 note that you need to put children tags within a parent tag as you see here 
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  )
 }
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,8 +1,18 @@
+// 1 index.js - we wont be really editing but this is the bridge between the component created in app.js 
+// and the web browser
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+// 2 if you notice, what will happen is that the final product will be injected into the INDEX.HTML
+// more specifically under the DOM element "Root"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
