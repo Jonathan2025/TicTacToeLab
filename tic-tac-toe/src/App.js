0fe2a9b95d1f189edbd1cab9aceb66d86c19406e
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 16:31:36 -0400

    moved the states to being on the parent level so that its passed to the children

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,6 +3,19 @@ import Square from "./components/Square"
 
 
  function Board() {
+
+  //13 lifting state up, now we want to check for a winner
+  // in order to do that, the board needs to know the state of the 9 Square components 
+  // How do we do this? 
+  // TO COLLECT DATA FROM MULTIPLE CHILDREN, DECLARE THE SHARED STATE IN THE PARENT COMPONENT INSTEAD - Which is the board
+
+  //14 now we creata an Array(9).fill(null) meaning we create an array in which all the sqaures are initially null 
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+
+
+
+
   //3 note that you need to put children tags within a parent tag as you see here 
   return (
     <>
@@ -14,20 +27,27 @@ import Square from "./components/Square"
       </div> */}
     
     {/* 10 now we dont need to add the props anymore because we have the useStates that will set the values of the sqaures */}
+    {/* <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div> */}
+
+    {/* 15 now we want to be able to pass the value prop down to each square that it renders */}
     <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
 
 
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -14,14 +14,17 @@ import { useState } from 'react';
 
 //6 now we need to pass in the value props
 // 12 now once we start using states we dont need the props
-function Square() {
+// 17 that means we will now need to add in props, because we moved the states to the PARENT level 
+function Square({value}) {
     //9// Remember that we used value below 
-    const [value, setValue] = useState(null);
+    // 16 now we can get rid of the state in teh INDIVIDUAL sqaure component
+    // const [value, setValue] = useState(null);
 
 
 
    //8 lets fill the square component with an X when we click on it, 
    // Declare a handleClick inside of the square
+   
    function handleClick() {
     //11 instead of console.log click we can change the state to setValue('X")
     console.log('clicked!');
